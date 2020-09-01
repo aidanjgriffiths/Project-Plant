beaba07be1a73b3f4b8fd9732202ad8721ec00fa
Miles Danswan <miles.danswan@gmail.com>
2020-09-01 17:29:22 +1000

    Added JQuery and QRCode Dependencies with Example

--- PlantifulWeb/View/index.php
+++ PlantifulWeb/View/index.php
@@ -3,10 +3,16 @@
     <html lang="en">
     <head>
         <meta charset="UTF-8">
+        <script src="../Dependencies/qrcode.js"></script>
         <title>Home</title>
     </head>
     <body>
     <h1>Hello From Body</h1>
+    <div id="qr-test">
+        <script type="text/javascript">
+            new QRCode(document.getElementById("qr-test"), "12345678910121242342353463645");
+        </script>
+    </div>
     </body>
     </html>
 <?php include './Footer.php'; ?>
