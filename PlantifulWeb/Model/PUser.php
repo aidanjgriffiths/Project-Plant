e54bb18c4a37eac11c23da857305719e4142b63e
Miles Danswan <miles.danswan@gmail.com>
2020-09-06 18:49:24 +1000

    Modified User Model

--- PlantifulWeb/Model/PUser.php
+++ PlantifulWeb/Model/PUser.php
@@ -1,24 +1,13 @@
-<?php include_once(dirname(__DIR__) . './Root/DatabaseConnector.php');
+<?php include_once(dirname(__DIR__) . '/Root/DatabaseConnector.php');
 
 class PUser
 {
-	private $user_id_pk, $web_connect_token_fk;
+	private $user_id_pk, $web_connect_token_pk_fk;
 
-	public function __construct($web_connect_token_fk, $user_id_pk = null)
+	public function __construct($user_id_pk = null, $web_connect_token_pk_fk = null)
 	{
 		$this->user_id_pk = $user_id_pk;
-		$this->web_connect_token_fk = $web_connect_token_fk;
-	}
-
-	public function add_puser()
-	{
-		$insert_puser_query = "CALL usp_insert_puser('{$this->web_connect_token_fk}');";
-		$result = query($insert_puser_query);
-
-		$last_inserted_id = mysqli_fetch_assoc($result)['Last_Inserted_Id'];
-		$this->user_id_pk = $last_inserted_id;
-
-		return $last_inserted_id;
+		$this->web_connect_token_pk_fk = $web_connect_token_pk_fk;
 	}
 
 	public static function read_all_puser()
@@ -29,29 +18,23 @@ class PUser
 
 		while ($row = mysqli_fetch_assoc($result))
 		{
-			array_push($pusers, new PUser($row['web_connect_token_fk'], $row['user_id_pk']));
+			array_push($pusers, new PUser($row['user_id_pk'], $row['web_connect_token_pk_fk']));
 		}
-		
+
 		return $pusers;
 	}
 
-	public static function read_puser($user_id_pk)
+	public static function read_puser($user_id_pk, $web_connect_token_pk_fk)
 	{
-		$read_puser_query = "CALL usp_read_puser('{$user_id_pk}');";
+		$read_puser_query = "CALL usp_read_puser('{$user_id_pk}', '{$web_connect_token_pk_fk}');";
 		$row = mysqli_fetch_assoc(query($read_puser_query));
-		$puser = new PUser($row['web_connect_token_fk'], $row['user_id_pk']);
+		$puser = new PUser($row['user_id_pk'], $row['web_connect_token_pk_fk']);
 		return $puser;
-	}
-
-	public function update_puser()
-	{
-		$update_puser_query = "CALL usp_update_puser('{$this->user_id_pk}', '{$this->web_connect_token_fk}');";
-		query($update_puser_query);
 	}
 
 	public function delete_puser()
 	{
-		$delete_puser_query = "CALL usp_delete_puser('{$this->old_user_id_pk}');";
+		$delete_puser_query = "CALL usp_delete_puser('{$this->old_user_id_pk}', '{$this->old_web_connect_token_pk_fk}');";
 		query($delete_puser_query);
 	}
 
@@ -66,14 +49,14 @@ class PUser
 			return null;
 		}
 	}
-	
+
 	public function __set($property, $value)
 	{
 		if (property_exists($this, $property))
 		{
 			$this->$property = $value;
 		}
-		
+
 		return $this;
 	}
 }
