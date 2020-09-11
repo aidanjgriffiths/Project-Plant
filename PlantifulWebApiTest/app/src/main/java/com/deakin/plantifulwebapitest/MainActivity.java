c2e6f2c5e973c62abf71afae8c3dc136dfbe8cdd
Miles Danswan <miles.danswan@gmail.com>
2020-09-11 22:12:13 +1000

    Modified Test Functionality to work with RQ API

--- PlantifulWebApiTest/app/src/main/java/com/deakin/plantifulwebapitest/MainActivity.java
+++ PlantifulWebApiTest/app/src/main/java/com/deakin/plantifulwebapitest/MainActivity.java
@@ -18,8 +18,10 @@ import android.view.MenuItem;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
@@ -34,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final String UAC_URL = "https://www.mdlproto.com/PlantifulWeb/Stem/UACStem.php";
+        final String RQ_URL = "https://www.mdlproto.com/PlantifulWeb/Stem/RQStem.php";
+
         /**
          * Create an Concurrent task for connecting to the Plantiful Website API
          */
@@ -45,22 +50,39 @@ public class MainActivity extends AppCompatActivity {
                 // request arguments
                 String uid = "100";
                 String wid = "200";
+                String rq = "SELECT * FROM PUser";
 
                 HttpURLConnection urlConnection = null;
 
                 try {
-                    URL url = new URL("http://www.mdlproto.com/Stem/UACStem.php");
+                    URL url = new URL(RQ_URL);
                     urlConnection = (HttpURLConnection) url.openConnection();
                     urlConnection.setRequestMethod("POST");
                     // write arguments to the output stream of HTTPUrlConnection
                     OutputStream outputStream = new BufferedOutputStream(urlConnection.getOutputStream());
                     BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, StandardCharsets.UTF_8));
-                    bufferedWriter.write("uac_w_uid=" + uid + "&uac_w_wid=" + wid);
+                    bufferedWriter.write("r_q_str=" + rq);
                     bufferedWriter.flush();
                     bufferedWriter.close();
                     outputStream.close();
                     urlConnection.connect(); // connect to website and execute HTTP POST request
-                    urlConnection.getResponseCode(); // recover the request code to ensure the request did not fail!
+                    int responseCode = urlConnection.getResponseCode(); // recover the request code to ensure the request did not fail!
+
+                    // get response from server
+                    StringBuilder sb = new StringBuilder();
+                    InputStream is = responseCode == 200 ? urlConnection.getInputStream() : urlConnection.getErrorStream();
+                    BufferedReader bis = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+                    String curLine = "";
+
+                    // retrieve each line from the response from the server
+                    while ((curLine = bis.readLine()) != null)
+                        sb.append(curLine + "\n");
+
+                    // close input streams
+                    is.close();
+                    bis.close();
+
+                    Log.d("Debug", sb.toString());
                 } catch (Exception e) {
                     Log.d("Debug", e.getMessage() == null ? "NULL MSG" + e.toString() : e.getMessage());
                 } finally {
