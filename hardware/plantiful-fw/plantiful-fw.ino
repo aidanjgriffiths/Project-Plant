4410729191dba5b457dad01aced417a1986ac017
AshPhillips2786 <69656570+AshPhillips2786@users.noreply.github.com>
2020-09-22 11:35:05 +1000

    Allow visual read indication (#37)
    * CHORE: Clean up folder structure
    
    Clean up and rename hardware folders prior to project completion.
    
    * FEAT: Add visual feedback on read
    
    Add visual feedback on sensor read request
    
    * CHORE: Clean out testing code
    
    Remove accidentally committed testing code.
    
    * CHORE: Improve code structure
    
    Swap from if to switch to allow easier future improvements.

--- hardware/plantiful-fw/plantiful-fw.ino
+++ hardware/plantiful-fw/plantiful-fw.ino
@@ -1,6 +1,8 @@
-#include <DHT.h>; //Adafruit DHT library
+#include <DHT.h> //Adafruit DHT library
 #include <SoftwareSerial.h>
-#define VERSION "0.1"
+#define VERSION "0.2"
+//Comment this out when NOT debugging the hardware.
+//#define DEBUG 1
 #define BLUETOOTH_RX 11
 #define BLUETOOTH_TX 10
 #define BLUETOOTH_BAUD 9600
@@ -13,6 +15,7 @@
 #define ADJ_SOIL_MAX 100
 #define ADJ_LIGHT_MIN 0
 #define ADJ_LIGHT_MAX 120000
+#define FEEDBACK_LED_PIN 13
 
 DHT dht(DHT_PIN, DHT22);
 SoftwareSerial bluetooth(BLUETOOTH_RX, BLUETOOTH_TX);
@@ -28,6 +31,14 @@ char buf[30];
 
 
 void setup() {
+  #ifdef DEBUG
+    Serial.begin(9600);
+    Serial.println("Debug Enabled");
+  #endif
+  
+  pinMode(FEEDBACK_LED_PIN, OUTPUT);
+  digitalWrite(FEEDBACK_LED_PIN, LOW);
+  
   bluetooth.begin(BLUETOOTH_BAUD);
   dht.begin();
   delay(2000); //This is required to give the DHT time to start/stabilise
@@ -42,33 +53,37 @@ void loop() {
     rawLight = min(RAW_LIGHT_MAX, rawLight);
     light = map(rawLight, RAW_LIGHT_MIN, RAW_LIGHT_MAX, ADJ_LIGHT_MIN, ADJ_LIGHT_MAX);
     moisture = map(rawMoisture, RAW_SOIL_MIN, RAW_SOIL_MAX, ADJ_SOIL_MIN, ADJ_SOIL_MAX);
+
     if (bluetooth.available())
     {
       char c = bluetooth.read();
       c = tolower(c);
-      if(c == 'v')
+      switch(c)
       {
-        strcpy(buf, VERSION);
-        strcat(buf, "\r\n");
-        bluetooth.print(buf);
-      }
-      else if (c == 'r')
-      {
-        char numbers[5];
-        
-        strcpy(buf, "T");
-        strcat(buf, itoa(temperature, numbers, 10));
-        strcat(buf, "H");
-        strcat(buf, itoa(humidity, numbers, 10));
-        strcat(buf, "L");
-        strcat(buf, ltoa(light, numbers, 10));
-        strcat(buf, "M");
-        strcat(buf, itoa(moisture, numbers, 10));
-        bluetooth.println(buf);
-      }
-      else
-      {
-        delay(100); 
+        case('v'):
+          strcpy(buf, VERSION);
+          strcat(buf, "\r\n");
+          bluetooth.print(buf);
+        case('r'):
+          digitalWrite(FEEDBACK_LED_PIN, HIGH);
+          delay(100);
+          digitalWrite(FEEDBACK_LED_PIN, LOW);
+          char numbers[5];
+          
+          strcpy(buf, "T");
+          strcat(buf, itoa(temperature, numbers, 10));
+          strcat(buf, "H");
+          strcat(buf, itoa(humidity, numbers, 10));
+          strcat(buf, "L");
+          strcat(buf, ltoa(light, numbers, 10));
+          strcat(buf, "M");
+          strcat(buf, itoa(moisture, numbers, 10));
+          
+          #ifdef DEBUG
+            Serial.println(buf);
+          #endif
+          
+          bluetooth.println(buf);
       }
     }
 }
